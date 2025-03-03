829147a93f012a814a0de183b2898422955e67c4
brenop2 <breno.p231@gmail.com>
2025-03-03 15:03:57 -0300

    Primeiro commit

---
+++ aprendendo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+  printf("Ola Mundo\n");
+    printf("Ola 2");
+    printf("Ola 3");
+    printf("Ola 2");
+    printf("Ola 2");
+    printf("Ola 1");
+    printf("Ola 2");
+    printf("Ola 2");
+    printf("Ola 2");
+  return 0;
+ }
