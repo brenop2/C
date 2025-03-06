886597fec03571e7968f7dea4c94d20d024dc8ef
brenop2 <breno.p231@gmail.com>
2025-03-06 16:59:02 -0300

    subindo

--- output/exercicios/ex1.c
+++ output/exercicios/ex1.c
@@ -1,41 +1,37 @@
 #include <stdio.h>
 
-int main(){
-  float salario_f, vl_hora; // tava errando pois estava terminando a determinação/atribuição das variaveis usando ;
-  int Aulas_trabalhadas;
-
-  //calculos salario
-  float dia_sal = vl_hora * 8;
-  float Total_de_salario = dia_sal * Aulas_trabalhadas;
 
   //calculo imposto
-  int INSS(salario){
-    float salario;
-    if (salario <= 1518,00) {
-      salario = salario * 100 - (salario / )
+  float desconto(float salario_des) {
+    if (salario_des <= 1518.00) {
+      salario_des = salario_des * 0.075;
+      printf("%.2f", salario_des);
+    } else if (salario_des >= 2793.88) {
+      salario_des = salario_des * 0.09;
     }
-
-
   }
+int main(){
+      float vl_hora; // tava errando pois estava terminando a determinação/atribuição das variaveis usando ;
+      int Aulas_trabalhadas;
 
-  printf("Digite o valor de sua hora: ");
-  scanf("%f", &vl_hora);
+      //calculos salario
+      float dia_sal = vl_hora * 8;
+      float Total_de_salario = dia_sal * Aulas_trabalhadas;
 
-
-  printf("Digite a quantidade de aulas que voce trabalhou: ");
-  scanf("%d", &Aulas_trabalhadas);
-
-
-  printf("Total de seu salario e R$: %.2f sem descontos/ ou seja bruto", Total_de_salario);
-
+      float resultado = Total_de_salario - desconto(Total_de_salario);
 
 
+      printf("Digite o valor de sua hora: ");
+      scanf("%.2f", &vl_hora);
 
 
-    return 0;
+      printf("Digite a quantidade de aulas que voce trabalhou: ");
+      scanf("%.2f", &Aulas_trabalhadas);
+
+      printf("Total de seu salario e R$: %.2f sem descontos/ ou seja bruto\n", Total_de_salario);
+
+      printf(" Seu salario liquido e: %.2f\n", resultado);
 
 
-
-
-
-  }
+      return 0;
+    }
